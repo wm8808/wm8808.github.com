f80be4c3299cd3b576522c7e8e65d04bb3c7c1cd
wangmeng <wangm8808@sina.com>
2016-03-27 11:07:08 +0800

    this is my web

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ window.onload = function (){
 		var oName = document.getElementById('name');
 		var aP = oName.children;
 		var oHc = document.getElementById('hc');
+		var oJs = document.getElementById('js');
+		var oHc3 = document.getElementById('hc3');
 		var iNow = 0;
 		var timer = null;
 		for( var i = 0; i<aP.length; i++){
@@ -14,7 +16,11 @@ window.onload = function (){
 		timer = setTimeout(function (){
 			clearInterval(timer);
 			move(oPage,{top:-oPage.offsetHeight},{duration:2000,complete:function (){
-				move(oHc,{left:160},{duration:2000});	
+				move(oHc,{left:160},{duration:800,complete:function (){
+					move(oJs,{right:650},{duration:1500,complete:function (){
+						move(oHc3,{right:160},{duration:1200});
+					}});	
+				}});	
 			}});
 		},3000);
 		
