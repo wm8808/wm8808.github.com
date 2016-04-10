d77c8fe99561d10c1b5c958ca8b40d8e6c4d0b54
wangmeng <wangm8808@sina.com>
2016-04-10 22:29:25 +0800

    my web

--- js/page2.js
+++ js/page2.js
@@ -15,6 +15,54 @@ window.onload = function (){
 		}
 		
 	})();
+	/*;(function (){
+		var oNav = document.getElementById('nav');
+		var oHtml = oNav.children[0];
+		var oJ2s = oNav.children[1];
+		var oH5 = oNav.children[2];
+		oHtml.onclick = function (){
+			window.open('html.html','_blank');
+			this.className = 'active';	
+		};
+		oJ2s.onclick = function (){
+			window.open('js.html','_blank');
+			this.className = 'active';	
+		};
+		oH5.onclick = function (){
+			window.open('html5.html','_blank');
+			this.className = 'active';	
+		};
+	})();*/
+	/*;(function (){
+		ajax(url,function(str){
+			var arr = eval('(+'str'+)');
+			
+				
+		},function(){
+			失败	
+		});
+		var oCenter = document.getElementById('center');
+		var oUl = document.getElementById('con');
+		var oNext = document.getElementById('next');	
+		oNext.onclick = function (){
+			ajax('content.txt?t=' +Math.random(),function (str){
+			var arr = eval('('+str+')');
+			arr.sort(function (){
+				return Math.random()-0.5;
+			});
+
+			alert(arr);
+				oUl.innerHTML = '';
+				for( var i = 0; i<8; i++){
+					var oLi = document.createElement('li');
+					oLi.innerHTML = '<a href="'+arr[i].href+'"><img src="'+arr[i].src+'" alt="" /><p>'+arr[i].cont+'</p></a>';
+					oUl.appendChild(oLi);	
+				}	
+			},function (){
+				alert('失败');	
+			});	
+		};	
+	})();*/
 	;(function (){
 		var oCon = document.getElementById('con');
 		var aCon = oCon.children;
@@ -25,23 +73,90 @@ window.onload = function (){
 			aCon[i].onmouseover = function (){
 				for( var i = 0; i<aCon.length; i++){
 					aCon[i].className = '';
-					//aImg[i].style.width = '240px';
-					//aImg[i].style.height = '135px';
+					aImg[i].style.width = '240px';
+					aImg[i].style.height = '135px';
 					//aP[i].style.marginTop = '20px';
 				}
 				this.className = 'show';
-				//aImg[this.index].style.width = '250px';
-				//aImg[this.index].style.height = '145px';
+				aImg[this.index].style.width = '250px';
+				aImg[this.index].style.height = '145px';
 				//aP[this.index].style.marginTop = '21px';
-					
 			};
-		}		
+			aCon[i].onmouseout = function (){
+				this.className = '';
+				for( var i = 0; i<aCon.length; i++){
+					aImg[i].style.width = '240px';
+					aImg[i].style.height = '135px';	
+				}
+			};
+		}
+				
 	})();
 	;(function (){
+		var oCon = document.getElementById('con');
+		var aLi = oCon.children;
 		var oPrev = document.getElementById('prev');
 		var oNext = document.getElementById('next');
+		var Num = 0;
 		oNext.onclick = function (){
-				
-		};	
-	})();	
+			Num++;
+			if(Num > 1){
+				Num = 1;	
+			}
+			move(oCon,{top:-452*Num},{duration:1000});	
+		};
+		oPrev.onclick = function (){
+			Num--;
+			if(Num == -1){
+				Num = 0;
+			}
+			move(oCon,{top:452*Num},{duration:1000});	
+		};			
+	})();
+	////翻页
+//	;(function (){
+//		var oCon = document.getElementById('con');
+//		var oPrev = document.getElementById('prev');
+//		var oNext = document.getElementById('next');
+//		var aLi = oCon.children;
+//		var oPos = [];
+//		//布局转换
+//		for( var i = 0; i<aLi.length; i++){
+//			oPos.push({left:aLi[i].offsetLeft,top:aLi[i].offsetTop});
+//		}
+//		console.log(oPos);
+//		for( var i = 0; i<aLi.length; i++){
+//			aLi[i].style.position = 'absolute';
+//			aLi[i].style.left = oPos[i].left+'px';
+//			aLi[i].style.top = oPos[i].top+'px';
+//			aLi[i].style.margin = 0;	
+//		}
+//		//布局转换结束
+//		var bOk = true;
+//		oNext.onclick = function (){
+//			if(!bOk)return;
+//			bOk = false;
+//			for( var i = 0; i<aLi.length; i++){
+//				(function (index){
+//					setTimeout(function (){
+//						move(aLi[index],{width:0, height:0,left:0,top:0,opacity:0},{complete:function (){
+//							//if(index == aLi.length - 1){
+////								for( var i = aLi.length-1;i>=0;i--){
+////									(function (index){
+////										setTimeout(function (){
+////											move(aLi[index],{left:oPos[index].left,top:oPos[index].top,width:272,height:180,opacity:1},{complete:function (){
+////												if(index == 0){
+////													bOk = true;
+////												}
+////											}});
+////										},100*(aLi.length-i));
+////									})(i);
+////								}
+////							}
+//						}});
+//					},100*i);
+//				})(i);
+//			}	
+//		};	
+//	})();	
 };
