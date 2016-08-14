e486c8a437fb08d879ed588031e0025e30756482
wangmeng <wangm8808@sina.com>
2016-08-14 17:26:26 +0800

    this is my web

---
+++ boot/js/index.js
@@ -0,0 +1,16 @@
+'use strict'
+$(window).load(function(){
+	$('.preloader').fadeOut(1000); // set duration in brackets    
+});
+$(function() {
+	new WOW().init();
+	$('.header-nav').singlePageNav({
+		offset: 70
+	});
+
+	/* Hide mobile menu after clicking on a link
+	-----------------------------------------------*/
+	$('.navbar-collapse a').click(function(){
+		$(".navbar-collapse").collapse('hide');
+	});
+})
